d6cfffd88cc661368cc674a3b31dac8ffd2ce85c
carreiranicolas <nicolascarreira2005@gmail.com>
2026-03-13 22:28:44 -0300

    Só adicionando os arquivos certinho igual que ele postou no portal. Se der erro, volte pra versão anterior

--- projetos-em-aula/projeto04-start/tasks.py
+++ projetos-em-aula/projeto04-start/tasks.py
@@ -1,5 +1,52 @@
 from invoke import task
 
+
 @task
-def install(c):
-    c.run('pip install -e ".[dev, test]"', echo = True)
+def install(c, dev=True):
+    """
+    Instala o projeto.
+    """
+    if dev:
+        c.run('pip install -e ".[dev,test]"', echo=True)
+    else:
+        c.run("pip install .", echo=True)
+
+
+@task
+def uninstall(c):
+    """
+    Remove o pacote instalado.
+    """
+    c.run("pip uninstall -y delivery", echo=True)
+
+
+@task
+def run(c):
+    """
+    Executa a aplicacao Flask.
+    """
+    c.run("flask run", pty=True)
+
+
+@task
+def test(c):
+    """
+    Executa os testes automatizados.
+    """
+    c.run("pytest tests -v", pty=True)
+
+
+@task
+def lint(c):
+    """
+    Verifica qualidade de codigo.
+    """
+    c.run("flake8", pty=True)
+
+
+@task
+def format(c):
+    """
+    Formata o codigo automaticamente.
+    """
+    c.run("black .", pty=True)
